ddec288a7d8135952af871c663c38a8b840e4779
kams <kams1011@naver.com>
2023-08-24 23:55:06 +0900

    init

---
+++ src/main/java/kr/toy/lyricsQuizServer/LyricsQuizServerApplication.java
@@ -0,0 +1,13 @@
+package kr.toy.lyricsQuizServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsQuizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsQuizServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/toy/lyricsQuizServer/LyricsQuizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.toy.lyricsQuizServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsQuizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
