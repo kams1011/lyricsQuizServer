24194c0ecf632940ef7ebfc78585284878a7ac44
kams <79346265+kams1011@users.noreply.github.com>
2024-04-13 20:43:01 +0900

    단일 프로젝트 배포 설정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  lintOnSave:false,
+  lintOnSave: false,
   transpileDependencies: true,
-  outputDir: "../src/main/resources/static",  // 빌드 타겟 디렉토리
+  outputDir: "../src/main/resources/static", // 빌드 타겟 디렉토리 설정
+
   devServer: {
     proxy: {
       '/api': {
-        // '/api' 로 들어오면 포트 80(스프링 서버)로 보낸다
+        // '/api'로 들어오면 포트 80(스프링 서버)로 보낸다
         target: 'https://localhost:80',
         changeOrigin: true, // cross origin 허용
       }
     },
     https: true
-  }
-})
+  },
+
+  configureWebpack: {
+    output: {
+      // Template for index.html
+      path: path.resolve(__dirname, '../src/main/resources/static'), // Vue 프로젝트의 빌드 결과물 위치 변경 설정
+    },
+  },
+
+  chainWebpack: config => {
+    // Modify the assets subdirectory and public path
+    config.plugin('html').tap(args => {
+      args[0].filename = path.resolve(__dirname, '../src/main/resources/static/index.html');
+      return args;
+    });
+  },
+});
