d46d619200a620c54613848b45a96fa959da5443
kams <79346265+kams1011@users.noreply.github.com>
2024-04-19 20:52:48 +0900

    commit

--- src/test/java/kr/toy/lyricsQuizServer/user/controller/UserControllerTest.java
+++ src/test/java/kr/toy/lyricsQuizServer/user/controller/UserControllerTest.java
@@ -1,38 +1,40 @@
-//package kr.toy.lyricsQuizServer.user.controller;
-//
-//import kr.toy.lyricsQuizServer.user.controller.port.UserService;
-//import kr.toy.lyricsQuizServer.user.domain.Role;
-//import kr.toy.lyricsQuizServer.user.domain.User;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.restdocs.RestDocumentationContextProvider;
-//import org.springframework.restdocs.RestDocumentationExtension;
-//import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import java.time.LocalDateTime;
-//
-//import static kr.toy.lyricsQuizServer.user.domain.LoginType.KAKAO;
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-//import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-//import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-//import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@ExtendWith(RestDocumentationExtension.class)    // JUnit5 필수
-//@WebMvcTest(UserController.class)
-//public class UserControllerTest {
-//
+package kr.toy.lyricsQuizServer.user.controller;
+
+import kr.toy.lyricsQuizServer.user.controller.port.UserService;
+import kr.toy.lyricsQuizServer.user.domain.Role;
+import kr.toy.lyricsQuizServer.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+
+import static kr.toy.lyricsQuizServer.user.domain.LoginType.KAKAO;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(RestDocumentationExtension.class)    // JUnit5 필수
+@WebMvcTest(UserController.class)
+@AutoConfigureRestDocs
+public class UserControllerTest {
+
 //    @Autowired
 //    private MockMvc mockMvc;
 //
@@ -90,7 +92,7 @@
 //
 //    }
 //
-//
-//
-//
-//}
+
+
+
+}
