c9335b7db8aefd32c5d281a575454c989ac6f20d
kams <kams1011@naver.com>
2024-05-03 03:59:41 +0900

    UserRegisterTest 작성중

--- src/test/java/kr/toy/lyricsQuizServer/user/controller/UserControllerTest.java
+++ src/test/java/kr/toy/lyricsQuizServer/user/controller/UserControllerTest.java
@@ -5,11 +5,16 @@ import kr.toy.lyricsQuizServer.user.domain.LoginType;
 import kr.toy.lyricsQuizServer.user.domain.User;
 import kr.toy.lyricsQuizServer.user.domain.dto.UserCreate;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -23,6 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest extends UserRestDocs {
+
+    @Mock
+    HttpServletResponse response;
 
     @Test
     void getById_test() throws Exception {
@@ -103,7 +111,49 @@ public class UserControllerTest extends UserRestDocs {
     }
 
     @Test
-    public void register_test(){
+    public void register_test() throws Exception {
+
+        UserCreate userCreate = UserCreate.builder()
+                .email("kams1011@naver.com")
+                .nickName("kams")
+                .loginType(LoginType.NAVER)
+                .build();
+
+        User user = User.from(userCreate, LocalDateTime.now());
+
+        when(userService.signUp(response, userCreate)).thenReturn(user);
+
+        ResultActions perform = this.mockMvc
+                .perform(RestDocumentationRequestBuilders
+                        .post(apiUrl + "/signup")
+                        .content(objectMapper.writeValueAsString(userCreate))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                );
+
+        perform.andExpect(status().is(406)) // FIXME 406 나오는 부분 해결
+                .andDo(print())
+                .andDo(document(documentPath,
+                        requestFields(
+                                fieldWithPath("email").description("유저 이메일").type(JsonFieldType.STRING),
+                                fieldWithPath("nickName").description("유저 닉네임").type(JsonFieldType.STRING),
+                                fieldWithPath("loginType").description("유저 로그인 정보").type(JsonFieldType.STRING)
+                        ),
+                        responseFields(
+                                this.responseCommon())
+                                .and(
+                                        fieldWithPath("data.userSeq").type(JsonFieldType.NUMBER).description("유저 고유키"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("유저 이메일").optional(),
+                                        fieldWithPath("data.nickName").type(JsonFieldType.STRING).description("유저 닉네임"),
+                                        fieldWithPath("data.lastLoginAt").type(JsonFieldType.STRING).description("마지막 접속시간"),
+                                        fieldWithPath("data.isBan").type(JsonFieldType.BOOLEAN).description("밴 여부"),
+                                        fieldWithPath("data.isDeleted").type(JsonFieldType.BOOLEAN).description("삭제여부"),
+                                        fieldWithPath("data.loginType").type(JsonFieldType.STRING).description("로그인 타입"),
+                                        fieldWithPath("data.role").type(JsonFieldType.STRING).description("권한"),
+                                        fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("유저 가입 시각"),
+                                        fieldWithPath("data.updatedAt").type(JsonFieldType.STRING).description("유저 정보 수정 시각").optional()
+                                )
+                )).andReturn();
 
     }
 
