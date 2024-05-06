4fc438e8a1427487c6e226fae7533a7f6d2cbedc
kams <79346265+kams1011@users.noreply.github.com>
2024-05-07 02:10:46 +0900

    commit

--- src/main/java/kr/toy/lyricsQuizServer/config/SecurityConfig.java
+++ src/main/java/kr/toy/lyricsQuizServer/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().permitAll();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
         http.cors();
         http
 //                .authorizeRequests()
