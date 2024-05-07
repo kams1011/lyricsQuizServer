2869e2baa7fd8f7cfb1c3ef60058f3af014e9eb3
kams <79346265+kams1011@users.noreply.github.com>
2024-05-08 05:41:16 +0900

    commit

--- src/main/java/kr/toy/lyricsQuizServer/config/ObjectMapperConfig.java
+++ src/main/java/kr/toy/lyricsQuizServer/config/ObjectMapperConfig.java
@@ -1,6 +1,7 @@
 package kr.toy.lyricsQuizServer.config;
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,7 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
